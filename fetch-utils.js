e98ddaf25c244c46af973d56a6d7dae229d9d2d8
Monika <moncobra@comcast.net>
2022-11-29 09:22:04 -0800

    some functions completed

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,33 +1,31 @@
-const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_URL = 'https://vmhclpevfecxhpxwubhs.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
-
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaGNscGV2ZmVjeGhweHd1YmhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI5Nzk4MzgsImV4cCI6MTk2ODU1NTgzOH0.pWvGlCrbKNRZWBKDRsPR8rGxu8nodj7nq8cY1rPNglI';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function createTodo(todo) {
+export async function createTodo(todos) {
+    const response = await client
+        .from('todos')
+        .insert({ todo: todos, complete: false, user_id: client.auth.user().id });
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
     // once you have a response from supabase, comment this back in:
-    // return checkError(response);
+    return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
-
     // once you have a response from supabase, comment this back in:
     // return checkError(response);
 }
--- render-utils.js
+++ render-utils.js
@@ -1,5 +1,7 @@
 export function renderTodo(todo, handleComplete) {
     // create a div and a p tag
+    const todoDiv = document.createElement('div');
+    const todoEl = document.createElement('p');
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
 
     // add the 'todo' css class no matter what
@@ -9,6 +11,6 @@ export function renderTodo(todo, handleComplete) {
     // append stuff
 
     // add event listener for click and call handleComplete function
-    
+
     // return the div
 }
